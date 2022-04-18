0307814bb7abb7d727089d2796d952166979ec1d
William-0320 <lisiyuan010320@hotmail.com>
2022-04-18 10:36:27 -0400

    Implemented all endpoints

--- server.js
+++ server.js
@@ -2,6 +2,8 @@
 const express = require('express');
 const app = express();
 
+var port = 5000;
+
 // Start an app server
 const server = app.listen(port, () => {
     console.log('App listening on port %PORT%'.replace('%PORT%',port))
@@ -21,6 +23,115 @@ app.get('/app/', (req, res) => {
         res.end(res.statusCode+ ' ' +res.statusMessage)
     });
 
+    app.get('/app/flip/', (req, res) => {
+        const result = coinFlip();
+        res.json({flip: result});
+    })
+
     app.get('/app/flips/:number', (req, res) => {
-        
+        const flips = coinFlips(req.params.number);
+        const counts = countFlips(flips);
+        res.json({raw: flips, summary: counts});
     });
+
+    app.get('app/flip/call/heads', (req, res) => {
+        res.send(flipACoin("heads"))
+    })
+
+    app.get('app/flip/call/tails', (req, res) => {
+        res.send(flipACoin("tails"))
+    })
+
+    /** Simple coin flip
+ * 
+ * Write a function that accepts no parameters but returns either heads or tails at random.
+ * 
+ * @param {*}
+ * @returns {string} 
+ * 
+ * example: coinFlip()
+ * returns: heads
+ * 
+ */
+
+function coinFlip() {
+    if(Math.random() < 0.5){
+      return "tails";
+    }else{
+      return "heads";
+    }
+  }
+  
+  /** Multiple coin flips
+   * 
+   * Write a function that accepts one parameter (number of flips) and returns an array of 
+   * resulting "heads" or "tails".
+   * 
+   * @param {number} flips 
+   * @returns {string[]} results
+   * 
+   * example: coinFlips(10)
+   * returns:
+   *  [
+        'heads', 'heads',
+        'heads', 'tails',
+        'heads', 'tails',
+        'tails', 'heads',
+        'tails', 'heads'
+      ]
+   */
+  
+  function coinFlips(flips) {
+    var result = [];
+    for(let i = 0; i < flips; i++){
+      result[i] = coinFlip();
+    }
+    return result;
+  }
+  
+  /** Count multiple flips
+   * 
+   * Write a function that accepts an array consisting of "heads" or "tails" 
+   * (e.g. the results of your `coinFlips()` function) and counts each, returning 
+   * an object containing the number of each.
+   * 
+   * example: conutFlips(['heads', 'heads','heads', 'tails','heads', 'tails','tails', 'heads','tails', 'heads'])
+   * { tails: 5, heads: 5 }
+   * 
+   * @param {string[]} array 
+   * @returns {{ heads: number, tails: number }}
+   */
+  
+  function countFlips(array) {
+    let numHeads = 0, numTails = 0;
+    for(let i = 0; i < array.length; i++){
+      if(array[i] === "heads"){
+        numHeads = numHeads + 1;
+      }else{
+        numTails = numTails + 1;
+      }
+    }
+    var result = {tails: numTails, heads: numHeads};
+    return result;
+  }
+  
+  /** Flip a coin!
+   * 
+   * Write a function that accepts one input parameter: a string either "heads" or "tails", flips a coin, and then records "win" or "lose". 
+   * 
+   * @param {string} call 
+   * @returns {object} with keys that are the input param (heads or tails), a flip (heads or tails), and the result (win or lose). See below example.
+   * 
+   * example: flipACoin('tails')
+   * returns: { call: 'tails', flip: 'heads', result: 'lose' }
+   */
+  
+  function flipACoin(call) {
+    var expected = coinFlip();
+    var outcome = "lose";
+    if(call === expected){
+      outcome = "win";
+    }
+    var result = {call: call, flip: expected, result: outcome};
+    return result;
+  }
